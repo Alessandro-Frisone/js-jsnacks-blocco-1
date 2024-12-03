ccdab3392f1ae6eff96606985e8e5f462751aad7
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-03 11:35:42 +0100

    feat: ex-1

---
+++ jsnack1/script.js
@@ -0,0 +1,9 @@
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore
+const num1 = prompt ("Inserisci un numero");
+const num2 = prompt ("Inserisci un numero");
+if (num1 > num2 ){
+    alert (num1)
+}else if (num2 > num1){
+    alert (num2)
+}
