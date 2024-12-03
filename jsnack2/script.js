aa1af1bedb5fe749b1c31ec03fe015e0f3943763
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-03 12:27:45 +0100

    feat: ex 2

---
+++ jsnack2/script.js
@@ -0,0 +1,16 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
+// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+const invitati = [Alessio, Luca, Davide, Marco, Simone];
+const nomeInvitato = prompt("Inserisci il tuo nome:");
+let puoPartecipare = false;
+for (let i = 0; i < invitati.length; i++) {
+    if (invitati[i] === nomeInvitato) {
+        puoPartecipare = true;
+    }
+}
+if (puoPartecipare) {
+    alert("Benvenuto alla festa!!!");
+} else {
+    alert("Mi dispiace, non sei nella lista degli invitati.");
+}
+
