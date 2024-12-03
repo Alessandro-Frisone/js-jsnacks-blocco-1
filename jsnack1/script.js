ae6fe9914870f46226060235f14fd723e52cc6be
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-03 11:50:48 +0100

    fix: number prompt

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,11 +1,11 @@
 // L’utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore
-const num1 = prompt ("Inserisci un numero");
-const num2 = prompt ("Inserisci un numero");
+const num1 = Number(prompt ("Inserisci un numero"));
+const num2 = Number(prompt ("Inserisci un numero"));
 if (num1 > num2 ){
     alert (num1)
 }else if (num2 > num1){
     alert (num2)
 }else {
-    alert ("i numeri sono uguali")
+    alert ("I numeri sono uguali")
 }
